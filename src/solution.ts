72cb0dce79e6f130d7996e7807da7fb45e78151a
keval chauhan <keval.chauhan0512@gmail.com>
2020-06-23 21:28:01 +0530

    change

--- src/solution.ts
+++ src/solution.ts
@@ -17,7 +17,7 @@ export function solution(D: Dictionary) {
         let current_date = new Date(keys[i]),
             next_date = new Date(keys[i + 1]);
 
-        // If there is a difference of more then one "date" i.e there is a gap between gap it goes inside
+        // If there is a difference of more then one "date" i.e if there is a gap between dates this logic runs
         if (1 < Math.ceil(Math.abs(next_date.getTime() - current_date.getTime()) / (1000 * 3600 * 24))) {
             gap = Math.ceil(Math.abs(next_date.getTime() - current_date.getTime()) / (1000 * 3600 * 24));
             var res = new Date(current_date.setDate(current_date.getDate() + 1));
